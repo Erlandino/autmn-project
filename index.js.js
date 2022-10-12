571927e4773e439fd2dc470faa67bf96f1effe3e
erlend <erlend.kodehode@gmail.com>
2022-10-12 14:17:27 +0200

    New folder structure

---
+++ index.js.js
@@ -0,0 +1,9 @@
+/*
+ *   this file contains code specific to
+ *   fetching the users name and
+ *   putting it in localstorage
+ *
+ * * * */
+
+console.log("Welcome to Solar Quiz")
+
---
+++ results-page/result.js
@@ -0,0 +1,6 @@
+/*
+ *   This file contains styling specific to the result page
+ *
+ * * * */
+
+ console.log("welcome to the result page")
