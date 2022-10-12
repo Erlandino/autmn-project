d275e5c7e9e4e9d4e14551d822dea64b705a783c
erlend <erlend.kodehode@gmail.com>
2022-10-12 18:14:23 +0200

    deleted som elements and created an elemnt trough js

--- quiz-page/quiz.js
+++ quiz-page/quiz.js
@@ -21,5 +21,22 @@
   </fieldset>
 */
 
-console.log("Welcome to the quiz part")
+console.log("Welcome to the quiz part");
 
+document.querySelector(".options").innerHTML = `
+          <div>
+            <input id="question0option0" name="question0" type="radio" value="0" autocomplete="off" required />
+            <label for="question0option0">Neptune</label>
+
+            <input id="question0option1" name="question0" type="radio" value="1" autocomplete="off" required />
+            <label for="question0option1">Mars</label>
+          </div>
+
+          <div>
+            <input id="question0option2" name="question0" type="radio" value="2" autocomplete="off" required />
+            <label for="question0option2">Saturn</label>
+
+            <input id="question0option3" name="question0" type="radio" value="3" autocomplete="off" required />
+            <label for="question0option3">Venus</label>
+          </div>
+`;
