61f432fb4a4e4d8fba791846245f9f9b4a20569f
erlend <erlend.kodehode@gmail.com>
2022-10-13 17:34:40 +0200

    added all files, couldnt get merge to work

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ *   this file contains code specific to
+ *   fetching the users name and
+ *   putting it in localstorage
+ *
+ * * * */
+
+console.log("Welcome to Solar Quiz");
+
+const nameInputHtml = document.querySelector("#nameInput");
+
+function sendToQuizPage() {
+  console.log(nameInputHtml.value);
+  if (nameInputHtml.value) {
+    localStorage.setItem("name", nameInputHtml.value);
+
+    window.location.href = "quiz-page/quiz.html";
+  }
+}
