c4f90cb2eb7b9731fa5e4630de4a42ffdfceb8aa
erlend <erlend.kodehode@gmail.com>
2022-10-13 16:19:00 +0200

    added function that adds name to local storage then refers them to ../quiz-page/quiz.html

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
  *
  * * * */
 
-console.log("Welcome to Solar Quiz")
+console.log("Welcome to Solar Quiz");
 
+const nameInputHtml = document.querySelector("#nameInput");
+
+function sendToQuizPage() {
+  console.log(nameInputHtml.value);
+  if (nameInputHtml.value) {
+    localStorage.setItem("name", nameInputHtml.value);
+
+    window.location.href = "../quiz-page/quiz.html";
+  }
+}
