4da4f578ae4e9a60bfb4c1f91f980d83f92c3424
Erlandino <110661323+Erlandino@users.noreply.github.com>
2022-10-14 14:42:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const errorNameHtml = document.querySelector("#errorName");
 // adds local storage name if there is any to the name input
 nameInputHtml.value = localStorage.getItem("name");
 
-// sends player to quiz page, adds name to local storage and reset score for local storage
+// sends player to quiz page, adds name to local storage, reset score for local storage, adds a erorr message to html if nameInputHtml.value is empty
 function sendToQuizPage() {
   if (nameInputHtml.value) {
     localStorage.setItem("name", nameInputHtml.value);
