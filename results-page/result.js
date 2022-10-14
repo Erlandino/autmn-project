926f44f9a15d1706b8cba44f83dd123d91ffa76c
erlend <erlend.kodehode@gmail.com>
2022-10-14 12:46:36 +0200

    added local storage score

--- results-page/result.js
+++ results-page/result.js
@@ -1,10 +1,8 @@
 // js code to extract winners name and winners core from local storage. commented out until connected to quiz page which sends local storage
 
-// const score = localStorage.getItem("score");
-
 // winners name and score stored in variables
 const playerName = localStorage.getItem("name");
-const score = 89 + "%";
+const score = localStorage.getItem("score");
 
 // html textcontent for 2 divs are replaced with winnerName and score variable
 document.querySelector(".playerName").textContent = playerName;
